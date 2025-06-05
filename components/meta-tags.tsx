a88181d33338bc7c28fa2b11910d98574031c975
emb91 <emmabardsley91@gmail.com>
2025-06-05 13:40:21 +1200

    Update favicons and meta tags, add new favicon assets

--- components/meta-tags.tsx
+++ components/meta-tags.tsx
@@ -4,27 +4,25 @@ export function MetaTags() {
   return (
     <Head>
       {/* Basic Meta Tags */}
-      <meta property="og:title" content="Arcova | Scientific Evidence for Business Decisions" />
+      <meta property="og:title" content="Arcova | We make science make sense" />
       <meta
         property="og:description"
-        content="Oxford-trained PhD team turning raw biomedical literature into decision-ready insight."
+        content="We help turn research into strategy, products, and progress."
       />
-      <meta property="og:image" content="/images/og-image.png" />
+      <meta property="og:image" content="/images/arcova-logo-transparent.png" />
       <meta property="og:type" content="website" />
 
-      {/* Twitter Card Tags */}
+      {/* Twitter Card Tags
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Arcova | Scientific Evidence for Business Decisions" />
       <meta
         name="twitter:description"
         content="Oxford-trained PhD team turning raw biomedical literature into decision-ready insight."
       />
-      <meta name="twitter:image" content="/images/og-image.png" />
+      <meta name="twitter:image" content="/images/og-image.png" /> */}
 
       {/* Favicon */}
-      <link rel="icon" href="/images/arcova-favicon.png" />
-      <link rel="apple-touch-icon" href="/images/arcova-favicon.png" />
-      <link rel="shortcut icon" href="/images/arcova-favicon.png" />
+      <link rel="icon" type="image/png" href="/arcova-favicon.png" sizes="any" />
 
       {/* Basic image tag that some platforms might use */}
       <link rel="image_src" href="/images/og-image.png" />
